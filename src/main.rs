f240be945a916f3af9d4e6882dbecb59a8b227d8
Jack Worman <66142217+jack-worman@users.noreply.github.com>
2024-12-02 14:59:00 -0500

    Day 2 (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod day_1;
+mod day_2;
 
 fn main() {
-    println!("{}", day_1::part_1("src/day_1/part_1.txt"));
-    println!("{}", day_1::part_2("src/day_1/part_2.txt"));
+    println!("Day 1, Part 1: {}", day_1::part_1("src/day_1/input.txt"));
+    println!("Day 1, Part 2: {}", day_1::part_2("src/day_1/input.txt"));
+    println!("Day 2, Part 1: {}", day_2::part_1("src/day_2/input.txt"));
+    println!("Day 2, Part 2: {}", day_2::part_2("src/day_2/input.txt"));
 }
