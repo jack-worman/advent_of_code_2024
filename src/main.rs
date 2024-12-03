56270b1d63cfcab035a9b3cdc16140c4657652bd
Jack Worman <jworman@healthcall.com>
2024-12-03 10:40:36 -0500

    Day 3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn main() {
     println!("Day 1, Part 1: {}", day_1::part_1("src/day_1/input.txt"));
     println!("Day 1, Part 2: {}", day_1::part_2("src/day_1/input.txt"));
+    println!();
     println!("Day 2, Part 1: {}", day_2::part_1("src/day_2/input.txt"));
     println!("Day 2, Part 2: {}", day_2::part_2("src/day_2/input.txt"));
+    println!();
+    println!("Day 3, Part 1: {}", day_3::part_1("src/day_3/input.txt"));
+    println!("Day 3, Part 2: {}", day_3::part_2("src/day_3/input.txt"));
 }
